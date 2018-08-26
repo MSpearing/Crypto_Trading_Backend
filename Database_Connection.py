1439be102296ac7817a3f8307f5f9e64352d6818
Michael Spearing <MSpearing@UTexas.edu>
2018-08-26 12:19:44 -0500

    Database_Connection.py: Refactoring how Database makes connection
    urls.py: Adding basic functionality

--- Database_Connection.py
+++ Database_Connection.py
@@ -1,30 +1,57 @@
 # Michael Spearing
 
+#TODO: Consider using SQLAlchemy instead of direct SQL
 import psycopg2 as pg
 import config
+
 class Database_Conection:
         
     def __init__(self):
         self.conn = pg.connect(database=config.db, user=config.username, password=config.password, host=config.host, port=config.port)
         self.cur = self.conn.cursor()
 
-    def get_Last_Entry(self, coin, base):
+    def get_Latest_Entry(self, coin, base):
         sql = "SELECT * FROM {}_{} ORDER BY timestamp DESC LIMIT 1".format(coin.lower(), base.lower())
-        self.cur.execute(sql)
-        return(self.to_json(self.cur.fetchone()))
+        return(self.execute_SQL(sql))
 
-    def get_Last_Entries(self, coin):
-        return
+    def get_Latest_Entries(self, coin, base, number):
+        sql = f'SELECT * FROM {coin.lower()}_{base.lower()} ORDER BY timestamp DESC LIMIT {number}'
+        return(self.execute_SQL(sql))
+
+    def get_Currencies(self):
+        sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
+        return(self.execute_SQL(sql))
+
+    def execute_SQL(self, sql):
+        self.cur.execute(sql)
+        rows = self.cur.fetchall()
+        column_names = [desc[0] for desc in self.cur.description]
+        if(type(rows[0]) == int):
+            tmp = []
+            tmp.append(rows)
+            rows = tmp
+        rows_formatted = []
+        for row in rows:
+            entry_dict = {}
+            index = 0
+            for column in column_names:
+                entry_dict[column] = row[index]
+                index += 1
+            rows_formatted.append(entry_dict)
+        return(rows_formatted)
 
     def __del__(self):
+        print('Removing DB Connecton')
         self.cur.close()
         self.conn.close()
+
+    def playground(self):
+        self.cur.execute('SELECT * FROM btc_usd LIMIT 2')
+        columns = [desc[0] for desc in self.cur.description]
+        return(columns)
     
-    def to_json(self, entry):
-        entry_dict = {'timestamp' : entry[0], 'ask_price' : entry[1], 'ask_size' : entry[2], 'ask_num_orders' : entry[3], 'bid_price' : entry[4], 'bid_size' : entry[5], 'bid_num_orders' : entry[6]}
-        return(entry_dict)
 
 if(__name__ == '__main__'):
     db = Database_Conection()
-    tmp = db.get_Last_Entry('btc', 'usd')
+    tmp = db.get_Latest_Entries('btc', 'usd', 5)
     print(tmp)
--- urls.py
+++ urls.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 db = Database_Connection.Database_Conection()
 
 @app.route('/home/<string:coin>_<string:base>/')
-def home_page(coin, base):
-    return(jsonify(db.get_Last_Entry(coin, base)))
+def get_Latest_Entry(coin, base):
+    return(jsonify(db.get_Latest_Entry(coin, base)))
+
+@app.route('/home/<string:coin>_<string:base>_<int:amt>/')
+def get_Latest_Entries(coin, base, amt):
+    return(jsonify(db.get_Latest_Entries(coin, base, amt)))
+
+@app.route('/home/currencies/')
+def get_Currencies():
+    return(jsonify(db.get_Currencies()))
 
 if(__name__ == '__main__'):
     app.run(debug=True)
